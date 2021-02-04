60d55fe2ddd1c5c0cb04627fc30bd99fd7d17905
Emanuel <e.rista@outlook.it>
2021-02-04 06:55:50 +0100

    upd

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,7 +12,7 @@
  
     <div class="card-body text-light d-flex flex-column justify-content-around align-items-center">
     <h5 class="card-title mb-2 text-center">{{ $post->title }}</h5>
-    <h6 class="card-title mb-2">{{ $post->author }}</h6>
+    <h6 class="card-title mb-2">di {{ $post->author }}</h6>
     
     <a href="{{ route("boolpress.show",$post->id) }}" class="btn btn-success">Leggi l'articolo</a>
   </div>
