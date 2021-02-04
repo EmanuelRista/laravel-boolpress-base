b42930e0ddc08b621aff564ce5cd84362eee4be1
Emanuel <e.rista@outlook.it>
2021-02-04 06:53:51 +0100

    lingua italiana

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,13 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\CategoryModel;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(CategoryModel::class, function (Faker $faker) {
     $title = implode(" ", $faker->words(2));
-    $slug = Str::slug($title, "-");
-    
+    $slug = Str::slug($title, "-"); //L'ho copiato :D
+
     return [
         "title" => $title,
         "slug" => "/" . $slug,
